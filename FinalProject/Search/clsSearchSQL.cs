7b21009bf0daf6ca81c18accafd205024d02809f
Rebecca <rebeccaalvey1@mail.weber.edu>
2017-11-21 16:06:43 -0700

    added in other classes and made some more changes

---
+++ FinalProject/Search/clsSearchSQL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Search
+{
+    class clsSearchSQL
+    {
+
+        static clsDataAccess db = new clsDataAccess();
+
+        public static DataSet allInvoiceNumbers()
+        {
+            DataSet myDS;
+            int iRet = 0;
+            string sSQL = "SELECT InvoiceNum FROM Invoices";
+            myDS = db.ExecuteSQLStatement(sSQL, ref iRet);
+            return myDS;
+        }
+    }
+}
