45b947b7314e85eda0493115646ea08d80fc3323
ElizabethAdams1 <beekeeper01@gmail.com>
2017-12-04 11:09:54 -0700

    Updating

--- FinalProject/Main/MainWindow.xaml.cs
+++ FinalProject/Main/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace FinalProject
     public partial class MainWindow : Window
     {
         Search.wndSearch wndSearchForm;
-        
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,16 +36,33 @@ namespace FinalProject
 
         }
 
-        private void edit_Click(object sender, RoutedEventArgs e)
+        private void btnEditInvoice_Click(object sender, RoutedEventArgs e)
         {
-            this.Hide();
-            EditPage ep = new EditPage();
-            ep.Show();
+            //this function will access a specified record (through txbInvNum) from the database.
+        }
+
+        private void btnDeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+            //this will delete a row from LineItems as specified by the invoice number, and LineItemNum accessed through txbInvNum and dgInvoiceItems by calling deleteInvoiceItem function in clsMainSQL
+        }
+
+        private void btnAddItem_Click(object sender, RoutedEventArgs e)
+        {
+            //this will add a row to LineItems as specified by the invoice number and LineItemNum accessed through txbInvNum and dgInvoiceItems by calling addInvoiceItem function in clsMainSQL
+
         }
 
         private void btnAddInvoice_Click(object sender, RoutedEventArgs e)
         {
-            
+            //this will add a row to the invoices table using the InvoiceDate from txbInvDate, the TotalCharge from txbInvTotal by calling the addInvoice function in clsMainSQL
         }
+
+        //cmbItems will be populated with Inventory data through allInventoryItems function in clsMainSQL
+
+        //txbItmeCost is a read only and will be populateed with Cost from ItemDesc in database using itemCost function in clsMainSQL
+
+        //txbInvTotal will be populated by adding all costs from dgInvoiceItems
+
+
     }
 }
